7e09a216ef026c32dd0d98c9dabeeef34b0a96d7
Alberto97 <albertop2197@gmail.com>
2016-11-10 16:59:34 +0100

    msm8916-common: Drop all the shims!!
    * Thanks Shr3ps and boulzordev team for providing updated libs
    
    Change-Id: Ie6e91370f2370b92ea8f1f487efc2dac6053590d

--- msm8916.mk
+++ msm8916.mk
@@ -78,7 +78,6 @@ PRODUCT_COPY_FILES +=  \
 PRODUCT_PACKAGES += \
     camera.msm8916 \
     libbson \
-    libshim_camera \
     Snap
 
 # Display
@@ -93,10 +92,6 @@ PRODUCT_PACKAGES += \
     ebtables \
     ethertypes \
     libebtc
-
-# DRM
-PRODUCT_PACKAGES += \
-    libshims_wvm
 
 # FM
 PRODUCT_PACKAGES += \
