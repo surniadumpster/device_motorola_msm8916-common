291fbdee38f6a80912abc3dc959351c2ee524131
ElDainosor <eldainosor@gmail.com>
2017-09-16 18:15:17 -0300

    idk #2
    taken some stuff from this, let's see if that does something.
    
    https://review.lineageos.org/#/c/187816/

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,14 +1,16 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := system/media/camera/include
+LOCAL_C_INCLUDES += \
+    framework/native/include \
+    system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_STATIC_LIBRARIES := libbase libarect
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils libbase libsensor \
+    libhardware liblog libcamera_client libutils libcutils libbase libsensor libhidltransport \
     android.hidl.token@1.0-utils \
     android.hardware.graphics.bufferqueue@1.0
 
