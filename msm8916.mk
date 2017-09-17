768739c485733f16fada2e6c5d199b3a4dd69d0b
DroidFreak32 <rushabshah32@gmail.com>
2017-09-17 22:07:07 +0530

    Shim libqomx_jpegenc.so and libmmqjpeg_codec with BoringSSL compatibility library

--- msm8916.mk
+++ msm8916.mk
@@ -107,7 +107,8 @@ PRODUCT_PACKAGES += \
     gralloc.msm8916 \
     copybit.msm8916 \
     hwcomposer.msm8916 \
-    memtrack.msm8916
+    memtrack.msm8916 \
+    libboringssl-compat
 
 # Ebtables
 PRODUCT_PACKAGES += \
