0e66ea3297dac805e2d5a47fa524aec3a7e6b63c
ElDainosor <eldainosor@gmail.com>
2017-10-04 23:47:57 +0000

    Revert "Shim libqomx_jpegenc.so and libmmqjpeg_codec with BoringSSL compatibility library"
    This reverts commit 768739c485733f16fada2e6c5d199b3a4dd69d0b.

--- msm8916.mk
+++ msm8916.mk
@@ -108,8 +108,7 @@ PRODUCT_PACKAGES += \
     gralloc.msm8916 \
     copybit.msm8916 \
     hwcomposer.msm8916 \
-    memtrack.msm8916 \
-    libboringssl-compat
+    memtrack.msm8916
 
 # Ebtables
 PRODUCT_PACKAGES += \
